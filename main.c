78f439beb43ca7d0f2abc625d5d4ef17e5e2ad2f
mmiguelo <marco_miguelote@hotmail.com>
2025-07-09 11:28:00 +0100

    changed layout of main to short lines

--- main.c
+++ main.c
@@ -1,23 +1,17 @@
-#include "cub3D.h"
+#include "includes/cub3D.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	//t_data	*data;
 
 	if (ac == 2)
 	{
-		if (access(av[1], F_OK | W_OK) == -1)
-		{
-			error_msg("File doesnt exist or there is no permitions!");
-			exit (1);
-		}
-		if ((check_directory(av[1]) == 0) && check_extension(av[1]) == 0)
-			//data = data_init(av[1]);
-			ft_printf_fd(1, "its ok");
+		parse(av[1]);
+		ft_printf_fd(1, G"congratulations\n"RST);
 	}
 	else if (ac != 2)
-		return (error_msg("No map input or too many arguments"), 1);
+		error_msg(ERR_ARGS);
 	else
-		return (error_msg("Invalid file!"), 1);
+		error_msg(ERR_FILE);
 	return (0);
 }
