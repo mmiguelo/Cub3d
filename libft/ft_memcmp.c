9efd32888f925aef0cda6da77fd3e2da2c91657e
mmiguelo <marco_miguelote@hotmail.com>
2025-07-09 11:26:53 +0100

    deleted comment main

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:51:32 by mmiguelo          #+#    #+#             */
-/*   Updated: 2024/10/30 14:26:05 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/07/09 10:04:50 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		i++;
 	return (str1[i] - str2[i]);
 }
-
-/* int	main(void)
-{
-	const char	*s1 = "Bom dia";
-	const char	*s2 = "Bom dia";
-	const char	*s3 = "Boa noite";
-	const char	*s4 = "Bom!";
-	size_t	n = 7;
-
-	printf("Comparing memory between s1 and s2 which are identical str:\n");
-	printf("%d", ft_memcmp(s1, s2, n)); //should be 0
-	printf("\nComparing memory between s1 and s3 which\
-	 are different strings:\n");
-	printf("%d", ft_memcmp(s1, s3, n)); // should be positive
-	printf("\nComparing memory between s1 and s4 which are different\
-	 lenghts (same content in first 3 chars):\n");
-	printf("%d", ft_memcmp(s1, s4, n)); // should be negative
-} */
