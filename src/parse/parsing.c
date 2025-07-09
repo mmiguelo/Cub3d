9f15e88b129f370261a30c51c707ca0b04f7d431
mmiguelo <marco_miguelote@hotmail.com>
2025-07-09 12:38:36 +0100

    changed kill function name

--- src/parse/parsing.c
+++ src/parse/parsing.c
@@ -3,7 +3,7 @@
 void	parse(char *filename)
 {
 	if (access(filename, F_OK | W_OK) == -1)
-		error_msg(ERR_FILE);
+		ft_kill(NULL, ERR_FILE);
 	check_directory(filename);
 	check_extension(filename);
 }
