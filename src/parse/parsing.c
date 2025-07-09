7bb24f5aa5c483e3d9e918edf1e8c8a329769c18
mmiguelo <marco_miguelote@hotmail.com>
2025-07-09 11:28:47 +0100

    added parse functions to aglomerate functions

--- src/parse/parsing.c
+++ src/parse/parsing.c
@@ -1,29 +1,9 @@
 #include "cub3D.h"
 
-int	check_extension(char *filename)
+void	parse(char *filename)
 {
-	int len;
-
-	len = ft_strlen(filename);
-	if (ft_strlen(filename) <= 4)
-		return (1);
-	else if (filename[len - 5] == '/')
-		return (1);
-	else if (ft_strncmp(filename + len - 4, ".cub", 4) != 0)
-		return (1);
-	return (0);
-}
-
-int	check_directory(char *filename)
-{
-	int	fd;
-
-	fd = open(filename, __O_DIRECTORY);
-	if (fd < 0)
-		return (0);
-	else
-	{
-		error_msg("Map sent is a directory!");
-		exit (0);
-	}
+	if (access(filename, F_OK | W_OK) == -1)
+		error_msg(ERR_FILE);
+	check_directory(filename);
+	check_extension(filename);
 }
