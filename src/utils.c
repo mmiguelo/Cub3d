e879d4c415f949feffb85cf7044eea88dd14d189
mmiguelo <marco_miguelote@hotmail.com>
2025-07-09 11:29:27 +0100

    added more error messages

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,29 @@
 #include "../includes/cub3D.h"
 
-void	error_msg(const char *error)
+const char	*message(t_error_code i)
 {
-	ft_printf_fd(2, RED"Error\n%s\n"RST, error);
-	return ;
+	const char	*message[12];
+
+	message[0] = "Arguments are invalid\n";
+	message[1] = "File must be *.cub\n";
+	message[2] = "Extension must be .cub\n";
+	message[3] = "File is invalid or no permissions\n";
+	message[4] = "Map is invalid\n";
+	message[5] = "Map sent is a directory\n";
+	message[6] = "Malloc failed\n";
+	message[7] = "Color is invalid\n";
+	message[8] = "Texture is invalid\n";
+	message[9] = "MLX failed\n";
+	message[10] = "Order is invalid\n";
+	message[11] = "Duplication found\n";
+	return (message[i]);
+}
+
+int	error_msg(t_error_code code)
+{
+	if (code >= 0 && code <= 12)
+		ft_printf_fd(2, RED"Error\n%s\n"RST, message(code));
+	else
+		ft_printf_fd(2, RED"Unkown Error\n"RST);
+	exit(EXIT_FAILURE);
 }
