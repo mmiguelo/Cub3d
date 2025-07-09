7961359b033df0e816db978726f27a4ec380e4d5
mmiguelo <marco_miguelote@hotmail.com>
2025-07-09 12:39:03 +0100

    changed kill function name

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 
 int	main(int ac, char **av)
 {
-	//t_data	*data;
+	t_data	*data;
+	t_textures	*texture;
 
 	if (ac == 2)
 	{
 		parse(av[1]);
+		ft_memset(&data, 0, sizeof(t_data));
+		ft_memset(&texture, 0, sizeof(t_textures));
 		ft_printf_fd(1, G"congratulations\n"RST);
 	}
 	else if (ac != 2)
-		error_msg(ERR_ARGS);
+		ft_kill(NULL, ERR_ARGS);
 	else
-		error_msg(ERR_FILE);
+		ft_kill(NULL, ERR_FILE);
 	return (0);
 }
