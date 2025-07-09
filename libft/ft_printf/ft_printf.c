c3b03e11f0378f7e59b06cc4fb5147a046c4cd78
mmiguelo <marco_miguelote@hotmail.com>
2025-07-09 11:27:10 +0100

    deleted comment main

--- libft/ft_printf/ft_printf.c
+++ libft/ft_printf/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 19:07:05 by mmiguelo          #+#    #+#             */
-/*   Updated: 2024/11/27 11:15:10 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/07/09 10:03:58 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,31 +51,3 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (count);
 }
-
-/* int main()
-{
-    char c = 'k'; // %c
-    char *str = NULL; // %s
-    
-    int nbr = -5;// %d // %i
-	char *var; var = str;// %p
-    unsigned int nbrr = 20;// %u
-    int b= 0xffffffff;// %x // %X
-    // %%
-    int i;
-	ft_printf("He%clo\n%s!\n%x\n%%", 'l', "World", -42);
-    printf("\n\nPrintf replica:\n");
-    i = ft_printf("| TEST TEXT |\n| Single Char: %c |\n| String: %s |\
-	\n| Pointer Adress: %p |\n| Decimal: %d |\n| Integer %i |\
-	\n| Unsign deci: %u |\n| HexLower: %x |\n| HexUpper: %X |\
-	\n| Percentage sign: %% |\n", c, str, var, nbr, nbr, nbrr, b, b);
-    printf("\nReturn Value: %d", i);
-    ///////////////////////////////
-    printf("\nPrintf original:\n");
-    i = printf("| TEST TEXT |\n| Single Char: %c |\n| String: %s |\
-	\n| Pointer Adress: %p |\n| Decimal: %d |\n| Integer %i |\
-	\n| Unsign deci: %u |\n| HexLower: %x |\n| HexUpper: %X |\
-	\n| Percentage sign: %% |\n", c, str, var, nbr, nbr, nbrr, b, b);
-    printf("\nReturn Value: %d\n", i);
-    return (0);
-} */
