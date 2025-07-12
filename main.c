b132736c7cdfe84a314f883f9b1e123adf2ab0fd
mmiguelo <marco_miguelote@hotmail.com>
2025-07-12 19:47:56 +0100

    changed main to reduce 5 lines

--- main.c
+++ main.c
@@ -4,18 +4,13 @@ int	main(int ac, char **av)
 {
 	t_data	data;
 
-	if (ac == 2)
-	{
-		parse(av[1]);
-		ft_memset(&data, 0, sizeof(t_data));
-		//init(&data); // calloc to structs inside t_data
-		//parse_file_content(&data, av[1]);
-		//mlx_functions();
-		ft_printf_fd(1, G"congratulations\n"RST);
-	}
-	else if (ac != 2)
+	if (ac != 2)
 		ft_kill(NULL, ERR_ARGS);
-	else
-		ft_kill(NULL, ERR_FILE);
+	parse(av[1]); // check file name, extension, directory, readability
+	ft_memset(&data, 0, sizeof(t_data)); // memset to structs inside t_data
+	init(&data); // calloc to structs inside t_data
+	parse_file_content(&data, av[1]);
+	//mlx_functions();
+	ft_printf_fd(1, G"\nCongratulations\n\n"RST);
 	return (0);
 }
