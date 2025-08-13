753a86bc16e46126a64f3d4cffe9c10970ca4a06
root <root@DESKTOP-V4EO9N9.localdomain>
2025-08-13 14:29:35 +0100

    changed include for bonus

--- src_bonus/game_bonus/game_loop_bonus.c
+++ src_bonus/game_bonus/game_loop_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3D.h"
+#include "cub3D_bonus.h"
 
 void	game_loop(t_data *data)
 {
