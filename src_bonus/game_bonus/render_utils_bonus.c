4264a849b46e16bb991bb3989146cd6b70373d14
root <root@DESKTOP-V4EO9N9.localdomain>
2025-08-14 23:03:10 +0100

    added files to light dinamically by time

---
+++ src_bonus/game_bonus/render_utils_bonus.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_utils_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/14 23:01:40 by mmiguelo          #+#    #+#             */
+/*   Updated: 2025/08/14 23:01:40 by mmiguelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3D_bonus.h"
+
+int	color(t_draw *draw, t_img *texture)
+{
+	return (*(int *)(texture->addr + (draw->tex_y * texture->line_length
+			+ draw->tex_x * (texture->bits_per_pixel / 8))));
+}
+
+void	put_pixel(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (y < 0 || y > WIN_HEIGHT - 1 || x < 0
+		|| x > WIN_WIDTH - 1)
+		return ;
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
