6ca1d9ed70dae8a4d8c0de26e21333332ec9f254
root <root@DESKTOP-V4EO9N9.localdomain>
2025-08-15 10:15:55 +0100

    changed return on ac!=2

--- src_bonus/main_bonus.c
+++ src_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 12:59:01 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/08/13 14:23:07 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/08/15 10:15:11 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int ac, char **av)
 	t_data	data;
 
 	if (ac != 2)
-		ft_kill(NULL, ERR_ARGS);
+		return (printf("%s", ERR_ARGS), 0);
 	init(&data);
 	parse(&data, av[1], ".cub");
 	parse_file_content(&data, av[1]);
